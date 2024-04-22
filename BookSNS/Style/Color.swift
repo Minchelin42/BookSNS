ae929a394fc58ac4d6935d2fdbd0d4cc018100db
Minchelin42 <mje0211@naver.com>
2024-04-23 00:49:29 +0900

    DESIGN: Color 추가

---
+++ BookSNS/Style/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  BookSNS
+//
+//  Created by 민지은 on 2024/04/23.
+//
+
+import UIKit
+
+enum Color {
+    static let mainColor = UIColor(named: "mainColor")
+    static let lightPoint = UIColor(named: "lightPoint")
+    static let pointColor = UIColor(named: "pointColor")
+}
